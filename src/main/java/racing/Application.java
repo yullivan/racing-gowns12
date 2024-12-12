43673084f8ba05c104e234b60e5feaf3dad1b397
정해준 <wjdgowns4414@gmail.com>
2024-12-12 13:16:58 +0900

    우승자 시상 구현

--- src/main/java/racing/Application.java
+++ src/main/java/racing/Application.java
@@ -1,10 +1,5 @@
 package racing;
 
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.util.List;
-
-@SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
